98ca0f0e393283982e87754d0f0f241eaf3707d5
Rene Enriquez <enriquezrene.ap@gmail.com>
2017-04-17 22:21:47 -0500

    Loops support

--- src/test/scala/io/gatling/demo/config/BasicHttpProtocol.scala
+++ src/test/scala/io/gatling/demo/config/BasicHttpProtocol.scala
@@ -5,9 +5,12 @@ import io.gatling.http.Predef._
 
 object BasicHttpProtocol {
 
+  val ghPwd: String = System.getenv("env.gh.pwd")
+
   val GitHubProtocolBuilder = http
     .baseURL("https://api.github.com")
     .userAgentHeader("enriquezrene")
     .acceptHeader("application/vnd.github.v3+json")
+    .basicAuth(username = "enriquezrene", password = ghPwd)
 
 }
